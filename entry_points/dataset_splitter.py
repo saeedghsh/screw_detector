5eec27bd3b84748e5a9e71487e4f79ef50090b08
Saeed Gholami Shahbandi <saeed.gh.sh@gmail.com>
2025-02-05 19:38:38 +0100

    Extend logging in data splitter enrty point
    Signed-off-by: Saeed Gholami Shahbandi <saeed.gh.sh@gmail.com>

--- entry_points/dataset_splitter.py
+++ entry_points/dataset_splitter.py
@@ -18,14 +18,28 @@ logger = setup_logging(name_appendix="data-splitter")
 def main(_: Sequence[str]) -> int:  # pragma: no cover
     """Main entry point for splitting and caching the dataset."""
     dataset_split_config = load_config("dataset_split")
+    logger.info(
+        "Splitting dataset into train and test sets with split ratio %.2f",
+        dataset_split_config["test_split_ratio"],
+    )
+
+    annotation_labels = {"screw_head", "screw_hole"}
     dataset_manager = DatasetManager()
     split_result = split(
         dataset_manager,
         frame_ids=list(dataset_manager.frame_ids.keys()),
-        annotation_labels={"screw_head", "screw_hole"},
+        annotation_labels=annotation_labels,
         subset_names=("test", "train"),
         desired_split_ratio=dataset_split_config["test_split_ratio"],
     )
+    for label in annotation_labels:
+        logger.info(
+            "Counts of label %s: train=%d, test=%d",
+            label,
+            split_result.stats["count"][label]["train"],
+            split_result.stats["count"][label]["test"],
+        )
+
     timestamp = datetime.now().strftime("%Y%m%dT%H%M%S")
     cache_dir_path = data_split_cache_path(ensure_exist=True)
     split_file = os.path.join(cache_dir_path, f"{timestamp}.json")
